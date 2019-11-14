7641da9363053a53c4711c4d7a889c55f70ee819
anlingxiao <1209882324@qq.com>
2019-11-14 17:25:23 +0800

    new Thread
    Signed-off-by: anlingxiao <1209882324@qq.com>

--- LearnThread.java
+++ LearnThread.java
@@ -2,4 +2,21 @@
  * Created by c06042 on 2019/11/14.
  */
 public class LearnThread {
+    public static void main(String[] args) {
+        System.out.println("main start...");
+        Thread t=new Thread(){
+            public void  run(){
+                System.out.println("thread run...");
+                try{
+                    Thread.sleep(10);
+                }catch (InterruptedException e){ }
+                System.out.println("thread end.");
+            }
+        };
+        t.start();
+        try{
+            Thread.sleep(20);
+        }catch (InterruptedException e){}
+        System.out.println("main end...");
+    }
 }
